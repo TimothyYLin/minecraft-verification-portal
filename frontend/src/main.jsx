ca25f69b1f678bdb8fa5831fca7961bdc8c4a999
Timothy Lin <timothylin1199@gmail.com>
2025-06-07 04:26:50 -0700

    Updated import statements to be absolute path rather than relative paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from './App.jsx'
-import HomePage from './pages/HomePage.jsx';
-import RegisterPage from './pages/RegisterPage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import DashboardPage from './pages/DashboardPage.jsx';
-import EmailVerificationRedirectPage from './pages/EmailVerificationRedirectPage.jsx';
-import NotFoundPage from './pages/NotFoundPage.jsx';
+import App from '@/App.jsx'
+import HomePage from '@/pages/HomePage.jsx';
+import RegisterPage from '@/pages/RegisterPage.jsx';
+import LoginPage from '@/pages/LoginPage.jsx';
+import DashboardPage from '@/pages/DashboardPage.jsx';
+import EmailVerificationRedirectPage from '@/pages/EmailVerificationRedirectPage.jsx';
+import NotFoundPage from '@/pages/NotFoundPage.jsx';
 
 import './index.css';
 
