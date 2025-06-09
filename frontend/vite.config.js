939a183f2de20469c0fa7a6ea4219cb4e109ed78
Timothy Lin <timothylin1199@gmail.com>
2025-06-09 13:38:47 -0700

    Updated vite.config.js to use https for secure cookie testing during development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl()
+  ],
+  server: {
+    https: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
   resolve: {
     alias:{
       '@': path.resolve(__dirname, './src'),
